c53f4b178d6ac949e7bd217f40dea9733b5bb2cd
Daniel Walters <dwdwdan@gmail.com>
2022-02-06 13:22:51 +0000

    Initial Commit

---
+++ init.el
@@ -0,0 +1,36 @@
+(setq inhibit-startup-message t)
+
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+(global-display-line-numbers-mode 1)
+(load-theme 'modus-vivendi t)
+
+;; Straight.el Bootstrap
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 5))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
+;; Configure straight
+(straight-use-package 'use-package)
+(setq straight-use-package-by-default t)
+
+(use-package evil
+  :config (evil-mode 1))
+
+(use-package helm
+  :bind
+  ("M-x" . helm-M-x)
+  ("C-x C-f" . helm-find-files)
+  :config (helm-mode 1))
+
+(use-package magit)
